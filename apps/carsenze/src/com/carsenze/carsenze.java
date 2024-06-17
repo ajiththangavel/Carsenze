3446d6cc7b12864820a8413fda2cbf2d2617bf6e
ajiththangavel <ajith.thangavel@randstaddigital.com>
2024-06-17 14:17:28 +0530

    updated the system app(Carsenze) Codes

--- apps/carsenze/src/com/carsenze/carsenze.java
+++ apps/carsenze/src/com/carsenze/carsenze.java
@@ -1,4 +1,4 @@
-package com.invcase;
+package com.carsenze;
 
 import android.content.Context;
 import android.app.Activity;
@@ -10,60 +10,68 @@ import android.widget.TextView;
 import android.util.Log;
 import android.os.ServiceManager;
 import android.os.IBinder;
-import android.hardware.invcase.IInvcase;
+import android.hardware.carsenze.ICarsenze;
 
-public class Invcase extends Activity {
-    private static final String TAG = "Invcase";
-    private static final String IINVCASE_AIDL_INTERFACE = "android.hardware.invcase.IInvcase/default";
-    private static IInvcase invcaseAJ; // AIDL Java
+public class Carsenze extends Activity {
+    private static final String TAG = "Carsenze";
+    private static final String IINVCASE_AIDL_INTERFACE = "android.hardware.carsenze.ICarsenze/default";
+    private static ICarsenze carsenzeAJ; // AIDL Java
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = (Button)findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button fetch = (Button)findViewById(R.id.button);
+        fetch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText editText = (EditText)findViewById(R.id.editText);
-                String txt = editText.getText().toString();
-                Log.d(TAG, "App: request= " + txt);
-
-                if(invcaseAJ != null) {
-                    try {
-                        invcaseAJ.putChars(txt);
-                    } catch (android.os.RemoteException e) {
-                        Log.e(TAG, "IInvcase-AIDL error", e);
-                    }
-                }
-
+                TextView mem = (EditText)findViewById(R.id.Memory);
+                TextView cpu = (EditText)findViewById(R.id.CPU);
+                TextView network = (EditText)findViewById(R.id.Network);
                 String ret = "";
 
-                if(invcaseAJ != null) {
+                if(carsenzeAJ != null) {
                     try {
-                        ret = invcaseAJ.getChars();
+                        ret = carsenzeAJ.getMemStats(txt);
+                        Log.d(TAG, "App: get= " + ret);
+                        mem.setText(ret);
                     } catch (android.os.RemoteException e) {
-                        Log.e(TAG, "IInvcase-AIDL error", e);
+                        Log.e(TAG, "ICarsenze-AIDL error", e);
                     }
                 }
 
-                Log.d(TAG, "App: get= " + ret);
+                if(carsenzeAJ != null) {
+                    try {
+                        ret = carsenzeAJ.getCpuStats(txt);
+                        Log.d(TAG, "App: get= " + ret);
+                        cpu.setText(ret);
+                    } catch (android.os.RemoteException e) {
+                        Log.e(TAG, "ICarsenze-AIDL error", e);
+                    }
+                }
 
-                TextView tv = (TextView)findViewById(R.id.textView);
-                tv.setText(ret);
+                if(carsenzeAJ != null) {
+                    try {
+                        ret = carsenzeAJ.getNetworkStats(txt);
+                        Log.d(TAG, "App: get= " + ret);
+                        network.setText(ret);
+                    } catch (android.os.RemoteException e) {
+                        Log.e(TAG, "ICarsenze-AIDL error", e);
+                    }
+                }
             }
         });
 
-        IBinder binder = ServiceManager.getService(IINVCASE_AIDL_INTERFACE);
+        IBinder binder = ServiceManager.getService(ICARSENZE_AIDL_INTERFACE);
         if (binder == null) {
-            Log.e(TAG, "Getting " + IINVCASE_AIDL_INTERFACE + " service daemon binder failed!");
+            Log.e(TAG, "Getting " + ICARSENZE_AIDL_INTERFACE + " service daemon binder failed!");
         } else {
-            invcaseAJ = IInvcase.Stub.asInterface(binder);
-            if (invcaseAJ == null) {
-                Log.e(TAG, "Getting IInvcase AIDL daemon interface failed!");
+            carsenzeAJ = ICarsenze.Stub.asInterface(binder);
+            if (carsenzeAJ == null) {
+                Log.e(TAG, "Getting ICarsenze AIDL daemon interface failed!");
             } else {
-                Log.d(TAG, "IInvcase AIDL daemon interface is binded!");
+                Log.d(TAG, "ICarsenze AIDL daemon interface is binded!");
             }
         }
     }
